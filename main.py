8fb008ebcbab21e7d97bd8b9cca7996efdb2918d
Gustavo Chichanoski <gustavo.chihcanoski@fkgroup.com.br>
2024-10-18 11:10:56 -0300

    Adicionado requisição para API Golang

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from scraping import Scraping
+import requests
 
 if __name__ == '__main__':
+    URL: str = "http://localhost:8090/api/v1/ordem_producao"
     S = Scraping()
-    S.init_scraping()
+    json_request: str = S.init_scraping()
+
+    headers: dict = {
+        'Content-Type': 'application/json'
+    }   
+
+    response = requests.post(URL, data=json_request, headers=headers)
+    if response.ok:
+        print(json_request)
+        print("Requisição post concluida, status code: ", response.status_code)
+    else:
+        print("Falha na requisição status code:  ", response.status_code)
